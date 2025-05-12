1606fbeb09b9a46bb1832fe36f9353af14926a7d
danzyxd <zmtkireev@gmail.com>
2025-05-12 20:15:07 +0300

    Builded cryptosystem of the application

--- L5101_Notepad/cryptography.py
+++ L5101_Notepad/cryptography.py
@@ -1,3 +1,6 @@
+from random import randint
+import configparser
+
 def is_prime(num):
     for i in range(2, round((num)**0.5) + 1):
         if num % i == 0:
@@ -8,8 +11,31 @@ def my_xor(text, key):
     key_str = str(key)
     return "".join(chr(ord(c) ^ ord(key_str[i % len(key_str)])) for i, c in enumerate(text))
 
+def generate_new_key():
+    while True:
+        new_key = randint(500_000, 1_000_000)
+        if is_prime(new_key):
+            return new_key
 
-# c = 0
-# for i in range(500_000, 1_000_000):
-#     if is_prime(i):
-#         c+=1
+def load_key():
+    config = configparser.ConfigParser()
+    config.read("AmTCD.ini")
+    key = int(config.get("main", "keyuser"), 16)
+    return key
+
+def encrypt(text):
+    key = load_key()
+    new_key = generate_new_key()
+    combined_key = key * new_key
+    encrypted_text = my_xor(text, new_key)
+    return f"{combined_key}\n{encrypted_text}"
+
+def decrypt(text):
+    key = load_key()
+    lines = text.split("\n", 1)
+    if len(lines) < 2:
+        raise ValueError("Неверный формат файла!")
+    combined_key = int(lines[0])
+    encrypted_text = lines[1]
+    new_key = combined_key // key
+    return my_xor(encrypted_text, new_key)
